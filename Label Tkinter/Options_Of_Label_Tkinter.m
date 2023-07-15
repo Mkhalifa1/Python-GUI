0bf1ed67e88cdc0fe972bfcfb7990ea96108b2fd
Mahmoud Khalifa Abdul Latif Hamed <63808216+Mkhalifa1@users.noreply.github.com>
2023-07-15 05:53:15 +0300

    Update Options_Of_Label_Tkinter.m

--- Label Tkinter/Options_Of_Label_Tkinter.m
+++ Label Tkinter/Options_Of_Label_Tkinter.m
@@ -1,8 +1,4 @@
-# Race results
-
-Here are the results from this week's race:
-
- | Number | Rider        | Time
- | 1      | Bullet Bob   | 56:07
- | 2      | Clumsy Colin | DNF 
- | 3      | Swift Susan  | 55:13
+| Command | Description |
+| --- | --- |
+| `git status` | List all *new or modified* files |
+| `git diff` | Show file differences that **haven't been** staged |
