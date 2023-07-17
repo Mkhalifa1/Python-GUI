6c710b5eace3bd4a028b489577254ad7d3d8420e
Mahmoud Khalifa Abdul Latif Hamed <63808216+Mkhalifa1@users.noreply.github.com>
2023-07-17 15:04:17 +0300

    Update Changing_icon.py

--- Tkinter Window/Changing_icon.py
+++ Tkinter Window/Changing_icon.py
@@ -1,10 +1,9 @@
-import tkinter as tk
+import tkinter as tk
 
-
-root = tk.Tk()
-root.title('Tkinter Window Demo')
+root = tk.Tk()
+root.title('Tkinter Window Demo')
 root.geometry('300x200+50+50')
-root.resizable(False, False)
+root.resizable(False, False)
 root.iconbitmap('./assets/python.ico')
 
 root.mainloop()
