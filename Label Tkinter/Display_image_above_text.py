62de0661eaefa8e51b93dec71bae89f7c31c5df7
Mahmoud Khalifa Abdul Latif Hamed <63808216+Mkhalifa1@users.noreply.github.com>
2023-07-20 03:23:49 +0300

    Update Display_image_above_text.py

--- Label Tkinter/Display_image_above_text.py
+++ Label Tkinter/Display_image_above_text.py
@@ -1,18 +1,19 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import ImageTk, Image
+
 
 # create the root window
 root = tk.Tk()
-root.geometry('300x200')
-root.resizable(False, False)
+root.geometry('1000x1000')
 root.title('Label Widget Image')
 
 # display an image label
-photo = tk.PhotoImage(file='./assets/python.png')
+photo = ImageTk.PhotoImage(Image.open("C:/Users/ENG-Mahmoud/Desktop/logo.png"))
 image_label = ttk.Label(
     root,
     image=photo,
-    text='Python',
+    text='Pharmacy ',
     compound='top'
 )
 image_label.pack()
